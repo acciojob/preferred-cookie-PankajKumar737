9cbb9472cec09ba24943658053c3595a907750b0
Pankaj Kumar <pankajkumar123014@gmail.com>
2023-04-24 18:57:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let fontSize = document.getElementById("fontsize");
+fontSize.addEventListener("change", () => {
+  fontSize.style.fontSize = fontSize.value;
+});
+let fontColor = document.getElementById("fontcolor");
+fontColor.addEventListener("input", () => {
+  fontColor.style.color = fontColor.value;
+});
+
+document.cookie = "fontSize.value; expires=Thu, 01 Jan 9999 00:00:00 UTC; path=/;";
+document.cookie= "fontColor.value; expires=Thu, 01 Jan 9999 00:00:00 UTC; path=/;";
